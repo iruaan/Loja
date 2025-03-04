b9bf206757915a700dad86a60d2eaf02690b4192
Ruan Santos <ruan.antoniu@gmail.com>
2025-03-03 22:52:33 -0300

    Alteração feita

--- src/main/java/com/loja/usecs/Controllers/PageController.java
+++ src/main/java/com/loja/usecs/Controllers/PageController.java
@@ -1,17 +1,30 @@
 package com.loja.usecs.Controllers;
 
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.loja.usecs.Model.Product;
+import com.loja.usecs.Repository.ProductRepository;
 
 
 
 @Controller
 public class PageController {
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    
     @GetMapping("/home")
-    public String home() {
-        return "Home";
+    public String home(Model model) {
+        List<Product> products = productRepository.findAll();
+        model.addAttribute("products", products); // aqui é products em inglês mesmo
+        return "Home"; // isso chama seu home.html
     }
     
     @GetMapping("/login")
--- src/main/java/com/loja/usecs/Repository/ProductRepository.java
+++ src/main/java/com/loja/usecs/Repository/ProductRepository.java
@@ -1,6 +1,8 @@
 package com.loja.usecs.Repository;
 
 import com.loja.usecs.Model.Product;
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
